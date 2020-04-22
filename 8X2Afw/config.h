435d1350f987c958e3a5366909b81df999a5b24f
Antoine Rousseau <_antoine_@metalu.net>
2020-04-22 16:43:33 +0200

    initial commit

---
+++ 8X2Afw/config.h
@@ -0,0 +1,30 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+
+//DCMotor config
+#define UINCPOW 10
+
+#define RAMP_UINCPOW 10
+#define RAMP_MAXERROR 1
+
+//#define MOTC_PWM 1
+//#define MOTD_PWM 2
+
+#define MOTC_END KZ1
+#define MOTC_ENDLEVEL 0
+#define MOTC_A KZ1
+#define MOTC_B KZ1
+
+#define MOTD_END KZ1
+#define MOTD_ENDLEVEL 0
+#define MOTD_A KZ1
+#define MOTD_B KZ1
+
+#define ANALOG_FILTER 5
+
+#define LAMP1 K6
+#define LAMP2 K7
+
+#endif // _CONFIG_H_
+
